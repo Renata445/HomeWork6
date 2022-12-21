4c1a4f14974d265b468de3a0751dc661137cfb75
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-21 20:00:52 +0300

    Task_41

---
+++ Task_41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите числа: ");
+int[] array = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int k = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    if(array[i] > 0)
+    {
+        k++;
+    }
+}
+Console.WriteLine(k);
