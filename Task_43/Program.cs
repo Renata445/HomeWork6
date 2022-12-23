c87acc9a302e3dbd960cce9d187ea75de41fd346
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-23 14:40:42 +0300

    Task_43

---
+++ Task_43/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите k1: ");
+double number_k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите k2: ");
+double number_k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b1: ");
+double number_b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите b2: ");
+double number_b2 = Convert.ToDouble(Console.ReadLine());
+double x, y;
+
+x = -(number_b1 - number_b2) / (number_k1 - number_k2);
+y = number_k1 * x + number_b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
